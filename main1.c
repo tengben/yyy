26ce9a97b6c122e3ee364e5b4facf50fa2e02ed8
Teng Ben <panliu1511@sina.com>
2011-04-06 20:46:14 +0800

    second

--- main1.c
+++ main1.c
@@ -2,16 +2,15 @@
 int main(void)
 {
     int d = 0;
-    int j = 0;
     int i = 0;
+    unsigned int mask = 0x80000000;
     puts("Please intut a number:'");
     scanf("%d",&d);
     puts("result:");
-    j = 31;
-    for(i=0;i<32;i++,j--)
+    for(i=0;i<32;i++)
     {
-        printf("%d",((d>>j) & 0x01));
-         
+        printf("%d",((d & mask) !=0?1:0));
+        mask >>=1;
     }
  
     printf("\n");
