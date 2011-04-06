7f494101cba020b8b48708af098dd2e7beffcffb
Teng Ben <panliu1511@sina.com>
2011-04-06 21:00:42 +0800

    3

--- main1.c
+++ main1.c
@@ -3,11 +3,11 @@ int main(void)
 {
     int d = 0;
     int i = 0;
-    unsigned int mask = 0x80000000;
+    unsigned int mask =(0x01<<(sizeof(int)*8-1)) ;
     puts("Please intut a number:'");
     scanf("%d",&d);
     puts("result:");
-    for(i=0;i<32;i++)
+    for(i=0;i<(sizeof(int)*8);i++)
     {
         printf("%d",((d & mask) !=0?1:0));
         mask >>=1;
