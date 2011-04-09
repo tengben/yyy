70a714cab61527b0c4ad8dc92a8ed053163c0379
Teng Ben <panliu1511@sina.com>
2011-04-09 20:32:39 +0800

    7

---
+++ i.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    int array[4][5];
+    int *p = array[0];
+    int (*s)[];
+    char **ptr;
+    int *x[10];
+    int i = 0;
+
+    ptr = argv;
+
+    for (i = 0; i < argc; i++) 
+    {
+        printf("%s\n",*(ptr+i));
+    }
+    return 0;
+
+}
