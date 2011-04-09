ebc7ac5ee91ab3f51857d95a8bfb0850c02fc480
Teng Ben <panliu1511@sina.com>
2011-04-09 19:40:20 +0800

    5

---
+++ g.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+#define N  100
+int main(int argc, const char *argv[])
+{
+    char str[N] = "  This  is  a  c  world !  ";
+    char *p = str;
+    char *s = str;
+
+    while (*s !='\0') 
+
+    {   
+        while(*s == ' ')s++;
+        if(p !=s)
+        {
+                *p = *s;
+        }
+        p++;
+        s++;
+       
+    }
+    *p = '\0';
+    printf("len = %d\t%s\n",strlen(str),str);
+
+    return 0;
+}
