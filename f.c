a6eaceeae8aa6b0d3c4feac324467baa47bf9335
Teng Ben <panliu1511@sina.com>
2011-04-09 19:39:21 +0800

    4

---
+++ f.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    char str[20]="This is a C world!";
+    char c_str[20];
+    char *p = str;
+    char *p_c = c_str;
+    for(p=str;*p !='\0';p++)
+    {
+            if(*p != ' ')
+            {
+                    *p_c = *p;
+                     p_c++;
+            }
+    }
+    *p_c = '\0';
+    printf("%s\n",c_str);
+
+    return 0;
+ }   
