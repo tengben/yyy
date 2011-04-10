a8e6a72b943ebf48dce2c9a055af24457fc0d541
Teng Ben <panliu1511@sina.com>
2011-04-10 21:08:20 +0800

    9

---
+++ l.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include<string.h>
+int string_to_number(char *s);
+
+int main(int argc, const char *argv[])
+{
+    char str[100];
+    int number = 0;
+    printf("Please input a string:\n");
+    scanf("%s,str");
+    number = string_to_number(str);
+    if(number == -1)
+    {
+        printf("NOnum\n");
+    }
+    else
+    {
+        printf("number= %d\n",number);
+    }
+    return 0;
+}
+int string_to_number(char *s)
+{
+        int number = 0;
+        if ((*s>='0')&&(*s<='9')) 
+        {
+            number = (*s-0x30);
+            s++;
+            while((*s>='0')&&(*s<='9')) 
+            {
+                    number *= 10;
+                    number += (*s-0x30);
+                    s++;
+            }
+        }
+        else
+        {
+                return -1;
+
+        }
+        return number;
+}
