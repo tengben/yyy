705eedf516e92316091cb044c2f010ef729dc3ef
Teng Ben <panliu1511@sina.com>
2011-04-19 20:47:13 +0800

    文件操作

---
+++ y.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(int argc, const char *argv[])
+{
+    FILE *fp;
+    char str[100]="hello world";
+    char str_1[100];
+    int num = 78;
+    int r_num = 0;
+
+    fp = fopen("text1","w+");
+    if(fp == NULL)
+    {
+        perror("open:");
+        exit(0);
+    }
+    //fprintf(fp, "%d %d\n",num,num+1);
+    fscanf(fp,"%d %d",&num,&r_num);
+    printf("num =%d r_num=%d\n",num,r_num);
+    fclose(fp);
+    return 0;
+}
